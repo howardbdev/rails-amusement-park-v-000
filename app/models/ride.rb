1f58409b9f9254cb283ccd693ea56a42d27a2b68
Howard DeVennish <howarddevennish@gmail.com>
2017-05-18 13:15:09 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,5 +1,35 @@
 class Ride < ActiveRecord::Base
   # write associations here
   belongs_to :attraction
-  belongs_to :user 
+  belongs_to :user
+
+  def take_ride
+    user = self.user
+    attraction = self.attraction
+
+    if too_short?(user, attraction) || too_broke?(user, attraction)
+      return "Sorry. You do not have enough tickets to ride the #{attraction.name}." if !too_short?(user, attraction)
+      return "Sorry. You are not tall enough to ride the #{attraction.name}." if !too_broke?(user, attraction)
+      return "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    end
+
+    update_user(user, attraction)
+  end
+
+  private
+
+  def too_short?(user, attraction)
+    user.height < attraction.min_height
+  end
+
+  def too_broke?(user, attraction)
+    user.tickets < attraction.tickets
+  end
+
+  def update_user(user, attraction)
+    user.tickets -= attraction.tickets
+    user.nausea += attraction.nausea_rating
+    user.happiness += attraction.happiness_rating
+    user.save
+  end
 end
