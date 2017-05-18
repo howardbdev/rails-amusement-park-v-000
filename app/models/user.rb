4ad013f2d154987bf2e517985402ea0aea18fa76
Howard DeVennish <howarddevennish@gmail.com>
2017-05-18 12:35:54 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,14 @@ class User < ActiveRecord::Base
 
   after_initialize :set_default_admin
 
+  def mood
+    if self.happiness > self.nausea
+      return "happy"
+    elsif self.nausea > self.happiness
+      return "sad"
+    end
+  end
+
   private
 
   def set_default_admin
