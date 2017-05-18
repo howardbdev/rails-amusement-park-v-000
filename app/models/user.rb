ed805f76b7aaf6106f865c4630db6838a895afbc
Howard DeVennish <howarddevennish@gmail.com>
2017-05-18 12:30:10 +0000

    set up models and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
   # write associations here
+  has_secure_password
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  after_initialize :set_default_admin
+
+  private
+
+  def set_default_admin
+    self.admin ||= false
+  end
 end
